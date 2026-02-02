bc07446e236e166d56205e78c34e9e6c1a523b60
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-02-02 13:43:15 +0100

    feat: init commit

---
+++ premake5.lua
@@ -0,0 +1,101 @@
+workspace "Project"
+    architecture "x64"
+
+    configurations { 
+        "Debug", 
+        "Release",
+        "Distribution"
+    }
+
+    startproject "ChessLite"
+
+--------------------------------------------------------
+-- Helper function for consistent directory structure
+--------------------------------------------------------
+function SetTargetAndObjDirs(projectName)
+    local root = _MAIN_SCRIPT_DIR
+
+    -- Executables and DLLs
+    targetdir(root .. "/build/bin/" .. projectName .. "/%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}")
+
+    -- Intermediate build files
+    objdir(root .. "/build/intermediates/" .. projectName .. "/%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}")
+
+    
+    --------------------------------------------------------
+    -- Automatically move all .lib files to centralized folder
+    --------------------------------------------------------
+    filter "kind:StaticLib"
+        targetdir(root .. "/build/lib/" .. projectName .. "/%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}")
+    filter {}
+end
+
+--------------------------------------------------------
+-- Helper for common configuration settings
+--------------------------------------------------------
+function ApplyCommonConfigs()
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
+        buildoptions { "/MTd" }
+        kind "ConsoleApp"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "Full"
+        buildoptions { "/MT" }
+        kind "ConsoleApp"
+
+    filter "configurations:Distribution"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "Full"
+        buildoptions { "/MT" }
+        kind "WindowedApp"
+
+    filter {} -- reset filter
+end
+
+------------------------------------
+-- Libraries Includes
+------------------------------------
+group "Libraries"
+    include "CoreLib"
+    include "SDLCoreLib"
+-- Restore default group
+group ""
+------------------------------------
+-- Examples Includes
+------------------------------------
+include "ChessLite"
+
+--------------------------------------------------------
+-- Custom clean action
+--------------------------------------------------------
+newaction {
+    trigger = "clean",
+    description = "Remove all binaries, intermediates, and VS files",
+    execute = function()
+        print("Removing binaries...")
+        os.rmdir("./build/bin")
+
+        print("Removing intermediates...")
+        os.rmdir("./build/intermediates")
+
+        print("Removing libraries...")
+        os.rmdir("./build/lib")
+        os.rmdir("./build")
+
+        print("Removing Visual Studio files...")
+        os.rmdir("./.vs")
+        os.rmdir("./.vscode")
+        os.remove("**.sln")
+        os.remove("**.vcxproj")
+        os.remove("**.vcxproj.filters")
+        os.remove("**.vcxproj.user")
+
+        print("Done.")
+    end
+}
