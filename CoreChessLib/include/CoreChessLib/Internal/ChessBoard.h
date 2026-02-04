2fa38a4effe0cf4d9cd4834d505755d20b61ea1d
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-02-04 22:18:21 +0100

    feat: field

--- CoreChessLib/include/CoreChessLib/Internal/ChessBoard.h
+++ CoreChessLib/include/CoreChessLib/Internal/ChessBoard.h
@@ -1,9 +1,29 @@
 #pragma once
+#include "ChessTypes.h"
 
 namespace CoreChess::Internal {
 
-	class Field {
+	enum class FieldType : uint32_t{
+		NONE = 0,
+		WHITE,
+		BLACK
+	};
 
+	class Field {
+	public:
+		Field() = default;
+		Field(ChessPieceID piece, FieldType type);
+
+		ChessPieceID GetPieceID() const;
+		FieldType GetFieldType() const;
+
+		bool IsPieceNone() const;
+		bool IsPieceWhite() const;
+		bool IsPieceBlack() const;
+
+	private:
+		ChessPieceID m_piece;
+		FieldType m_type = FieldType::NONE;
 	};
 
 	class ChessBoard {
@@ -13,6 +33,7 @@ namespace CoreChess::Internal {
 	private:
 		int m_boardSizeX = 0;
 		int m_boardSizeY = 0;
+		std::vector<Field> m_board;
 	};
 
 }
--- CoreChessLib/src/CoreChessLib/Internal/ChessBoard.cpp
+++ CoreChessLib/src/CoreChessLib/Internal/ChessBoard.cpp
@@ -2,6 +2,30 @@
 
 namespace CoreChess::Internal {
 
+	Field::Field(ChessPieceID piece, FieldType type)
+		: m_piece(piece), m_type(type) {
+	}
+
+	ChessPieceID Field::GetPieceID() const {
+		return m_piece;
+	}
+
+	FieldType Field::GetFieldType() const {
+		return m_type;
+	}
+
+	bool Field::IsPieceNone() const {
+		return m_type == FieldType::NONE;
+	}
+
+	bool Field::IsPieceWhite() const {
+		return m_type == FieldType::WHITE;
+	}
+
+	bool Field::IsPieceBlack() const {
+		return m_type == FieldType::BLACK;
+	}
+
 	ChessBoard::ChessBoard(int x, int y) 
 		: m_boardSizeX(x), m_boardSizeY(y) {
 	}
