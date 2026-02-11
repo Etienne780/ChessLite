2d8fcdf6a8a8f69406133959f6465a074b2661e4
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-02-11 13:29:12 +0100

    fix: missing '/' char

--- Server/premake5.lua
+++ Server/premake5.lua
@@ -17,13 +17,13 @@ project "Server"
     includedirs {
         "include",
         "include/%{prj.name}",
-        "%{wks.location}vendor/mysql/include",
-        "%{wks.location}vendor/SDL3_net/include"
+        "%{wks.location}/vendor/mysql/include",
+        "%{wks.location}/vendor/SDL3_net/include"
     }
 
     libdirs {
-        "%{wks.location}vendor/mysql/lib/x64",
-        "%{wks.location}vendor/SDL3_net/lib/x64"
+        "%{wks.location}/vendor/mysql/lib/x64",
+        "%{wks.location}/vendor/SDL3_net/lib/x64"
     }
 
     links {
