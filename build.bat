62284055d5d70c205926a7159eaffe9c46dc964f
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-02-27 00:18:06 +0100

    feat: show tablles

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
 @echo off
 
+set DB_USER=root
+set DB_PASS=root
+set DB_HOST=127.0.0.1
+set DB_NAME=game
+
 IF "%~1" == "" GOTO PrintHelp
 IF "%~1" == "help" GOTO PrintHelp
 IF "%~1" == "compile" GOTO Compile
@@ -7,6 +12,7 @@ IF "%~1" == "db-start" GOTO DbStart
 IF "%~1" == "db-stop" GOTO DbStop
 IF "%~1" == "db-status" GOTO DbStatus
 IF "%~1" == "db-restart" GOTO DbRestart
+IF "%~1" == "db-tables" GOTO DbTables
 
 vendor\premake5\premake5.exe %1
 GOTO Done
@@ -18,25 +24,13 @@ echo Enter 'build.bat [action]' where action is one of the following:
 echo.
 echo   clean             Remove all binaries and intermediate binaries and project files.
 echo   compile           Will generate make file then compile using the make file.
-echo   codelite          Generate CodeLite project files
-echo   gmake             Generate GNU makefiles for POSIX, MinGW, and Cygwin
-echo   gmakelegacy       Generate GNU makefiles for POSIX, MinGW, and Cygwin
-echo   vs2005            Generate Visual Studio 2005 project files
-echo   vs2008            Generate Visual Studio 2008 project files
-echo   vs2010            Generate Visual Studio 2010 project files
-echo   vs2012            Generate Visual Studio 2012 project files
-echo   vs2013            Generate Visual Studio 2013 project files
-echo   vs2015            Generate Visual Studio 2015 project files
-echo   vs2017            Generate Visual Studio 2017 project files
-echo   vs2019            Generate Visual Studio 2019 project files
-echo   vs2022            Generate Visual Studio 2022 project files
-echo   xcode4            Generate Apple Xcode 4 project files
-echo   -----------------------------------------------------------
+echo   ...
 echo   DB:
 echo   db-start	         Starts the MySQL DB
 echo   db-stop           Stops the MySQL DB
 echo   db-status         Returns the status of the DB
 echo   db-reset          Stops, removes volumes, and starts the DB fresh
+echo   db-tables         Lists all tables in the game database
 GOTO Done
 
 :Compile
@@ -53,11 +47,21 @@ GOTO Done
 :DbStart
 echo Starting Docker database...
 docker compose up -d
+
+echo Waiting for MySQL to be ready...
+:WaitLoop
+docker exec game-db mysqladmin ping -u %DB_USER% -p%DB_PASS% --silent > nul 2>&1
+IF ERRORLEVEL 1 (
+    timeout /t 2 > nul
+    GOTO WaitLoop
+)
+
+echo MySQL is ready.
 GOTO Done
 
 :DbStop
 echo Stopping Docker database...
-docker compose down
+docker compose down -v
 GOTO Done
 
 :DbStatus
@@ -69,6 +73,11 @@ GOTO Done
 echo Restarting Docker database...
 docker compose down
 docker compose up -d
+GOTO Done
+
+:DbTables
+echo Showing tables in database %DB_NAME%...
+docker exec -it game-db mysql -u %DB_USER% -p%DB_PASS% -e "USE %DB_NAME%; SHOW TABLES;"
 GOTO Done
 
 :Done
