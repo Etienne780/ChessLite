d7429ee020f472384acd796a1602dbb7ee287f61
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-02-27 12:54:26 +0100

    feat: server test

--- Game/ChessLite/src/App.cpp
+++ Game/ChessLite/src/App.cpp
@@ -39,11 +39,11 @@ void App::OnStart() {
 void App::OnUpdate() {
     ConnectClient();
 
-    if (SDLCore::Time::GetFrameCount() % 200) {
-        m_context.gameClient.Send("ChessLite aura", 
-        [](bool result, const std::string& msg) {
-            Log::Print("Server msg: result={}; msg={};", result, msg);
-        });
+    if (SDLCore::Time::GetFrameCount() % 1000) {
+        m_context.gameClient.Send("ChessLite Aura FPS " + static_cast<int>(SDLCore::Time::GetFrameRateHzF()),
+            [](bool result, const std::string& msg) {
+                Log::Print("Server msg: result={}; msg={};", result, msg);
+            });
     }
 
     if (!m_winID.IsInvalid()) {
--- Server/src/ServerLogic/GameServerLogic.cpp
+++ Server/src/ServerLogic/GameServerLogic.cpp
@@ -9,7 +9,14 @@ GameServerLogic::GameServerLogic(NetServer* server)
 }
 
 void GameServerLogic::OnMessage(NET_StreamSocket* client, const std::string& msg) {
+    if (!client || msg.size() < sizeof(uint32_t))
+        return;
 
+    std::string message = "Server: " + msg;
+
+    int sent = NET_WriteToStreamSocket(client, message.c_str(), static_cast<int>(message.size()));
+    if (sent < 0)
+        std::cerr << "Failed to send echo: " << SDL_GetError() << "\n";
 }
 
 void GameServerLogic::OnServerMessage(const std::string& serverName, const std::string& msg) {
